cea5f1127c167e831bcb9a30bf4c5bb361d89e50
catchonme <izengdp@163.com>
2018-09-20 16:04:28 +0800

    two sum

---
+++ 001-two-sum.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]:
+        """
+        look_up = {}
+        for i, num in enumerate(nums):
+            if target - num in look_up:
+                return [look_up[target-num], i]
+            look_up[num] = i
+        return []
+
+
+sol = Solution()
+res = sol.twoSum([2, 7, 11, 15], 18)
+print(res)
