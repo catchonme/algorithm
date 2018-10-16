306109a87f7a2465fba79a5d1a6925c022f14f4d
catchonme <izengdp@163.com>
2018-10-16 09:38:50 +0800

    queue

---
+++ JavaScript/data-structures/queue/Queue.js
@@ -0,0 +1,33 @@
+import LinkedList from '../linked-list/LinkedList'
+
+
+export default class Queue {
+    constructor() {
+        this.linkedList = new LinkedList();
+    }
+
+    isEmpty() {
+        return !this.linkedList.head;
+    }
+
+    peek() {
+        if (!this.linkedList.head) {
+            return null;
+        }
+
+        return this.linkedList.head.value;
+    }
+
+    enqueue() {
+        this.linkedList.append(value);
+    }
+
+    dequeue() {
+        const removeHead = this.linkedList.deleteHead();
+        return removeHead ? removeHead.value : null;
+    }
+
+    toString() {
+        return this.linkedList.toString(callback);
+    }
+}
