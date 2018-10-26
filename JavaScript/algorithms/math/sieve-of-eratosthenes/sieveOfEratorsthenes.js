88cff82af0d78b21bfe27d4d5f864104a1b0ef91
catchonme <izengdp@163.com>
2018-10-26 08:44:52 +0800

    sieve of sratosthenes

---
+++ JavaScript/algorithms/math/sieve-of-eratosthenes/sieveOfEratorsthenes.js
@@ -0,0 +1,25 @@
+export default function sieveOfEratosthenes(maxNumber) {
+    const isPrime = new Array(maxNumber + 1).fill(true);
+    isPrime[0] = false;
+    isPrime[1] = false;
+
+    const primes = [];
+
+    for (let number = 2; number <= maxNumber; number += 1) {
+        if (isPrime[number] === true) {
+            primes.push(number);
+
+            let nextNumber = number * number;
+
+            while (nextNumber <= maxNumber) {
+                isPrime[nextNumber] = false;
+                nextNumber += number;
+            }
+        }
+    }
+
+    return primes;
+}
+
+let res = sieveOfEratosthenes(25);
+console.log(res)
