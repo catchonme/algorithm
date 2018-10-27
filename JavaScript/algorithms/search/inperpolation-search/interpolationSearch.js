5d2e09eef69d8b1e16475e7ecb0f60ee32ab5687
catchonme <izengdp@163.com>
2018-10-27 19:30:03 +0800

    interpolation search

---
+++ JavaScript/algorithms/search/inperpolation-search/interpolationSearch.js
@@ -0,0 +1,33 @@
+export default function interpolationSearch(sortedArray, seekElement) {
+    let leftIndex = 0;
+    let rightIndex = sortedArray.length - 1;
+
+    while (leftIndex <= rightIndex) {
+        const rangeDelta = sortedArray[rightIndex] - sortedArray[leftIndex];
+        const indexDelta = rightIndex - leftIndex;
+        const valueDelta = seekElement = sortedArray[leftIndex];
+
+        if (valueDelta < 0) {
+            return -1;
+        }
+
+        if (!rangeDelta) {
+            return sortedArray[leftIndex] === seekElement ? leftIndex : -1;
+        }
+
+        const middleIndex = leftIndex + Math.floor(valueDelta * indexDelta / rangeDelta);
+
+        if (sortedArray[middleIndex] === seekElement) {
+            return middleIndex;
+        }
+
+        if (sortedArray[middleIndex] < seekElement) {
+            leftIndex = middleIndex + 1;
+        } else {
+            rightIndex = middleIndex - 1;
+
+        }
+    }
+
+    return -1;
+}
